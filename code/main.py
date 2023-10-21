a90b5c40637dd0906dc976cf3f4d06f80ec705af
Daniel Oliveira <danielsantosoliveira753@gmail.com>
2023-10-21 19:10:13 -0300

    Move o codigo de preprocessamento para outro lugar

--- code/main.py
+++ code/main.py
@@ -1,78 +1,217 @@
+import json
 import pickle
 
-import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 import pydotplus
-from sklearn.metrics import classification_report, precision_score
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.preprocessing import LabelEncoder
+import scipy.stats as st
+from sklearn.base import RegressorMixin
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score, precision_score, recall_score, make_scorer
+from sklearn.model_selection import GridSearchCV, KFold, cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_graphviz
-from sklearn.model_selection import KFold
+from yellowbrick import ROCAUC
+from yellowbrick.classifier import ConfusionMatrix, ClassificationReport, PrecisionRecallCurve
 
 
-def categorize_sleep_efficiency(efficiency):
-    if efficiency < 0.7:
-        return 'Low Efficiency'
-    elif 0.7 <= efficiency < 0.85:
-        return 'Medium Efficiency'
-    else:
-        return 'High Efficiency'
+# Function for calculating confidence interval from cross-validation
+def interval_confidence(values):
+    return st.t.interval(confidence=0.95, df=len(values) - 1, loc=np.mean(values), scale=st.sem(values))
 
 
-def extract_hour_and_minutes(data, column_name):
-    bedtime = pd.to_datetime(data[column_name])
-    return bedtime.dt.hour + bedtime.dt.minute / 60.0
+def cross_val(model, x_train, y_train, scorer):
+    kf = KFold(n_splits=10, shuffle=True, random_state=0)
+    scores = cross_val_score(model, x_train, y_train, cv=kf, scoring=scorer)
+    std = scores.std()
+    mean_score = scores.mean()
+    ic = interval_confidence(scores)
+    return std, mean_score, ic, scores
 
 
-def preprocessing():
-    data = pd.read_csv("../Sleep_Efficiency.csv")
-    # Remocao da Coluna ID
-    data = data.drop('ID', axis=1)
+def compute_metrics(model, x_train, y_train):
+    precision_scorer_not_disease = make_scorer(precision_score, labels=[0], average='micro')
+    precision_scorer_disease = make_scorer(precision_score, labels=[2], average='micro')
 
-    # Remocao de valores nulos
-    data = data.dropna()
+    recall_scorer_not_disease = make_scorer(recall_score, pos_label=0, average='micro')
+    recall_scorer_disease = make_scorer(recall_score, pos_label=1, average='micro')
 
-    # Converta as colunas de strings para datetime
-    data['Bedtime'] = pd.to_datetime(data['Bedtime'])
-    data['Wakeup time'] = pd.to_datetime(data['Wakeup time'])
+    f1_scorer_not_disease = make_scorer(f1_score, pos_label=0, average='micro')
+    f1_scorer_disease = make_scorer(f1_score, pos_label=1, average='micro')
 
-    # Crie as características de hora e minuto para "Bedtime"
-    data['Bedtime_hour_minute'] = extract_hour_and_minutes(data, "Bedtime")
+    precision_not_disease_std, precision_not_disease_mean, precision_ic_not_disease, precision_not_disease_values = \
+        cross_val(model, x_train, y_train, precision_scorer_not_disease)
+    precision_disease_std, precision_disease_mean, precision_ic_disease, precision_disease_values = \
+        cross_val(model, x_train, y_train, precision_scorer_disease)
 
-    # Crie as características de hora e minuto para "Wakeup time"
-    data['Wakeup_hour_minute'] = extract_hour_and_minutes(data, "Wakeup time")
+    recall_not_disease_std, recall_not_disease_mean, recall_ic_not_disease, recall_not_disease_values = \
+        cross_val(model, x_train, y_train, recall_scorer_not_disease)
+    recall_disease_std, recall_disease_mean, recall_ic_disease, recall_disease_values = \
+        cross_val(model, x_train, y_train, recall_scorer_disease)
 
-    # Drop as colunas originais, se desejar
-    data = data.drop(columns=['Bedtime', 'Wakeup time'])
+    f1_not_disease_std, f1_not_disease_mean, f1_ic_not_disease, f1_not_disease_values = \
+        cross_val(model, x_train, y_train, f1_scorer_not_disease)
+    f1_disease_std, f1_disease_mean, f1_ic_disease, f1_disease_values = \
+        cross_val(model, x_train, y_train, f1_scorer_disease)
 
-    data['Sleep efficiency Category'] = data['Sleep efficiency'].apply(categorize_sleep_efficiency)
+    metrics = {
+        'precision_not_disease_values': precision_not_disease_values.tolist(),
+        'precision_not_disease_std': precision_not_disease_std,
+        'precision_not_disease_mean': precision_not_disease_mean,
+        'precision_not_disease_ic': precision_ic_not_disease,
+        'precision_disease_values': precision_disease_values.tolist(),
+        'precision_disease_std': precision_disease_std,
+        'precision_disease_mean': precision_disease_mean,
+        'precision_disease_ic': precision_ic_disease,
+        'recall_not_disease_values': recall_not_disease_values.tolist(),
+        'recall_not_disease_std': recall_not_disease_std,
+        'recall_not_disease_mean': recall_not_disease_mean,
+        'recall_not_disease_ic': recall_ic_not_disease,
+        'recall_disease_values': recall_disease_values.tolist(),
+        'recall_disease_std': recall_disease_std,
+        'recall_disease_mean': recall_disease_mean,
+        'recall_disease_ic': recall_ic_disease,
+        'f1_not_disease_values': f1_not_disease_values.tolist(),
+        'f1_not_disease_std': f1_not_disease_std,
+        'f1_not_disease_mean': f1_not_disease_mean,
+        'f1_not_disease_ic': f1_ic_not_disease,
+        'f1_disease_values': f1_disease_values.tolist(),
+        'f1_disease_std': f1_disease_std,
+        'f1_disease_mean': f1_disease_mean,
+        'f1_disease_ic': f1_ic_disease
+    }
 
-    le_gender = LabelEncoder()
-    le_smoking_status = LabelEncoder()
-
-    data["Gender"] = le_gender.fit_transform(data["Gender"])
-    data["Smoking status"] = le_smoking_status.fit_transform(data["Smoking status"])
-
-    # Caso haja duplicados, remova-os
-    data = data.drop_duplicates()
-
-    x_train, x_test, y_train, y_test = train_test_split(
-        data.drop(columns=["Sleep efficiency", "Sleep efficiency Category"]),
-        data["Sleep efficiency Category"],
-        test_size=0.3,
-    )
-
-    print(x_train.columns.to_list())
-
-    # Salvando os conjuntos de dados em um arquivo pickle
-    with open("sleep_train_test.pkl", "wb") as f:
-        pickle.dump((x_train.columns.to_list(), x_train.values, x_test.values, y_train.values, y_test.values), f)
+    return metrics
 
 
-def train_decision_tree(x_train, y_train):
-    model = DecisionTreeClassifier()
+def fit_and_evaluate(model, x_train, x_test, y_train, y_test, feature_names):
     model.fit(x_train, y_train)
-    return model
+
+    if isinstance(model, RegressorMixin):
+        print("Oh yeah")
+
+    test_score = model.score(x_test, y_test)
+    print(f"Test score {model.__class__.__name__}", test_score)
+    metrics = compute_metrics(model, x_train, y_train)
+    y_pred = model.predict(x_test)
+
+    precision = precision_score(y_test, y_pred, average='micro')
+    recall = recall_score(y_test, y_pred, average='micro')
+    f1 = f1_score(y_test, y_pred, average='micro')
+
+    # accuracy = accuracy_score(y_test, y_pred)
+
+    class_report = ClassificationReport(model, classes=["Low Efficiency", "Medium Efficiency", "High Efficiency"])
+    class_report.fit(x_train, y_train)
+    class_report.score(x_test, y_test)
+    class_report.show(outpath=f"{model.__class__.__name__}_class_report.png", clear_figure=True)
+
+    prc = PrecisionRecallCurve(model, classes=["Low Efficiency", "Medium Efficiency", "High Efficiency"])
+    prc.fit(x_train, y_train)
+    prc.score(x_test, y_test)
+    prc.show(outpath=f"{model.__class__.__name__}_precision_recall_curve.png", clear_figure=True)
+
+    roc = ROCAUC(model, classes=["Low Efficiency", "Medium Efficiency", "High Efficiency"])
+    roc.fit(x_train, y_train)
+    roc.score(x_test, y_test)
+    roc.show(outpath=f"{model.__class__.__name__}_roc.png", clear_figure=True)
+
+    cm = ConfusionMatrix(model, classes=["Low Efficiency", "Medium Efficiency", "High Efficiency"])
+    cm.fit(x_train, y_train)
+    cm.score(x_test, y_test)
+    cm.show(outpath=f"{model.__class__.__name__}_confusion_matrix.png")
+
+    plt.close()
+
+    if isinstance(model, DecisionTreeClassifier):
+        visualize_tree(model, feature_names)
+
+    results = {
+        'model_name': model.__class__.__name__,
+        'test_score': test_score,
+        'precision': precision,
+        'recall': recall,
+        'f1_score': f1
+    }
+    results.update(metrics)
+
+    results_json = json.dumps(results)
+    return results_json
+
+
+def decision_tree_grid_search():
+    # open train and test sets
+    with open('sleep_train_test.pkl', 'rb') as f:
+        feature_names, x_train, x_test, y_train, y_test = pickle.load(f)
+
+        param_grid = {
+            'max_depth': [None, 5, 10, 15, 20],
+            'min_samples_split': [2, 5, 10],
+            'min_samples_leaf': [1, 2, 4],
+            'criterion': ['gini', 'entropy'],
+        }
+
+        # Perform a grid search with cross-validation
+        grid_search = GridSearchCV(
+            DecisionTreeClassifier(random_state=20),
+            param_grid=param_grid,
+            cv=5,
+            scoring='accuracy',
+            n_jobs=-1
+        )
+
+        # Fit the grid search object to the training data
+        grid_search.fit(x_train, y_train)
+
+        # Print the best hyperparameters and the corresponding score
+        print("Best hyperparameters for DecisionTree:", grid_search.best_params_)
+        print("Best score for DecisionTree:", grid_search.best_score_)
+
+        # Train a decision tree classifier on the training set
+        dtc_model = DecisionTreeClassifier(**grid_search.best_params_, random_state=20)
+        return fit_and_evaluate(dtc_model, x_train, x_test, y_train, y_test, feature_names)
+
+
+def decision_tree():
+    # open train and test sets
+    with open('sleep_train_test.pkl', 'rb') as f:
+        feature_names, x_train, x_test, y_train, y_test = pickle.load(f)
+        dtc_model = DecisionTreeClassifier(
+            criterion='entropy',
+            max_depth=5,
+            min_samples_leaf=1,
+            min_samples_split=2,
+            random_state=20
+        )
+        return fit_and_evaluate(dtc_model, x_train, x_test, y_train, y_test, feature_names)
+
+
+def random_forest():
+    # open train and test sets
+    with open('sleep_train_test.pkl', 'rb') as f:
+        feature_names, x_train, x_test, y_train, y_test = pickle.load(f)
+        rfc_model = RandomForestClassifier(
+            n_estimators=100,
+            min_samples_split=2,
+            min_samples_leaf=4,
+            max_features='log2',
+            criterion='entropy',
+            random_state=20
+        )
+
+        results = fit_and_evaluate(rfc_model, x_train, x_test, y_train, y_test, feature_names)
+
+        importances = rfc_model.feature_importances_
+
+        # Sort the features by importance in descending order
+        indices = importances.argsort()[::-1]
+
+        # Print the feature ranking
+        print("Feature ranking:")
+        for i in range(x_train.shape[1]):
+            print("%d. feature %s (%f)" % (i + 1, feature_names[indices[i]], importances[indices[i]]))
+
+        return results
 
 
 def visualize_tree(model, feature_names):
@@ -88,20 +227,13 @@ def visualize_tree(model, feature_names):
     graph.write_png(f"{model.__class__.__name__}_graph.png")
 
 
-
-
 if __name__ == '__main__':
-    preprocessing()
-    with open("sleep_train_test.pkl", "rb") as f:
-        feature_names, x_train_data, x_test_data, y_train_data, y_test_data = pickle.load(f)
-
-    tree_model = train_decision_tree(x_train_data, y_train_data)
-
-    y_pred = tree_model.predict(x_test_data)
-
-    print("Classification Report:")
-    print(classification_report(y_test_data, y_pred))
-    # evaluate_model(tree_model, x_test_data, y_test_data)
-
-
-    visualize_tree(tree_model, feature_names)
+    decision_tree_grid_search()
+# tree_results = decision_tree()
+# forest_results = random_forest()
+#
+# results = [tree_results, forest_results]
+#
+# with open('results.json', 'w') as f:
+#     # Use json.dump to write the results list to a file
+#     json.dump(results, f, indent=4)
